3165ba4e2c39a3d43a9c3b8a4a503c100f871e66
stefanbauer <alternative2k@googlemail.com>
2021-08-09 19:35:58 +0200

    wasd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import rnmCharacters from "./characters-response.json";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const allCharacters = rnmCharacters.results;
+
+  //filter: all living humans
+  const getAllLivingHumans = (allCharacters) =>
+    allCharacters.filter(
+      (char) => char.species === "Human" && char.status === "Alive"
+    );
+  console.log(getAllLivingHumans(allCharacters));
+
+  //filter: all males (gender)
+  const getAllMaleCharacters = (allCharacters) =>
+    allCharacters.filter((males) => males.gender === "Male");
+  console.log(getAllMaleCharacters(allCharacters));
+
+  //map: get all character names
+  const getAllNames = (allCharacters) =>
+    allCharacters.map((characterName) => characterName.name);
+  console.log("all names: ", getAllNames(allCharacters));
+
+  //map: get all names and origin - returned das Object origin
+  const getAllNamesAndOrigin = (allCharacters) =>
+    allCharacters.map((character) => {
+      return {
+        name: character.name,
+        origin: character.origin.name,
+      };
+    });
+  console.log("all names and origin: ", getAllNamesAndOrigin(allCharacters));
+
+  //map: get all names and origin - returned das Object origin
+  // mit runden Klammern statt return!
+  const getAllNamesAndOrigin2 = (allCharacters) =>
+    allCharacters.map((character) => ({
+      name: character.name,
+      origin: character.origin.name,
+    }));
+  console.log("all names and origin: ", getAllNamesAndOrigin2(allCharacters));
+
+  //map: get name and origin and all non-humans
+  const getNameAndOriginAndNonHumans = (allCharacters) =>
+    allCharacters.map((char) => {
+      if (char.species !== "Human") {
+        return {
+          name: char.species,
+          origin: char.origin.name,
+          species: char.species,
+        };
+      }
+    });
+  console.log(
+    "all names and origins of non-humans: ",
+    getNameAndOriginAndNonHumans(allCharacters)
   );
+
+  return <section></section>;
 }
 
 export default App;
